cb89cb5e6e84e69529f80c707ac3d3c2bac02582
sebastian <sebastian.waldschmidt-2@mni.thm.de>
2023-05-07 11:42:47 +0200

    Added functionality to navigate with a simple navigation bar. Added screens for placeholders so we can see that its working. Tested in Livepreview - No tests written

--- App.js
+++ App.js
@@ -1,14 +1,81 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+
+//Import navigation
+import 'react-native-gesture-handler';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+// Import screen components
 import Template from "./screens/Template";
+import HomeScreen from './screens/Home';
+import LiveMap from './screens/LiveMap';
+import Settings from "./screens/Settings";
+import NewPost from "./screens/NewPost";
+
+const Tab = createBottomTabNavigator();
+
+
+
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<StatusBar style="auto" />
-			<Text>Hola!</Text>
-			<Template />
-		</View>
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+						//HOME ICON
+						if (route.name === 'Home') {
+							iconName = focused ? 'home' : 'home-outline';
+						}
+						//TEMPLATE ICON
+						if (route.name === 'Template') {
+							iconName = focused ? 'reader' : 'reader-outline';
+						}
+						//NEW POST ICON
+					 	if (route.name === 'NewPost') {
+							iconName = focused ? 'add' : 'add-outline';
+						}
+					 	//LIVE MAP ICON
+						 if (route.name === 'LiveMap') {
+							iconName = focused ? 'map' : 'map-outline';
+						}
+						//SETTINGS ICON
+						 if (route.name === 'Settings') {
+							iconName = focused ? 'settings' : 'settings-outline';
+						}
+
+						// You can return any component that you like here!
+						return <Ionicons name={iconName} size={size} color={color} style={{ marginTop: focused ? -15 : 0 }} />;
+					},
+					tabBarLabelStyle: {
+						fontSize: 12,
+					},
+					tabBarStyle: {
+						paddingTop: 5,
+					},
+				})}
+				tabBarOptions={{
+					activeTintColor: 'blue',
+					inactiveTintColor: 'gray',
+					tabBarItemStyle: ({ focused }) => ({
+						marginTop: focused ? -5 : 0,
+					}),
+				}}
+			>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Template" component={Template} />
+				<Tab.Screen name="NewPost" component={NewPost} />
+				<Tab.Screen name="LiveMap" component={LiveMap} />
+				<Tab.Screen name="Settings" component={Settings} />
+			</Tab.Navigator>
+		</NavigationContainer>
 	);
 }
 
