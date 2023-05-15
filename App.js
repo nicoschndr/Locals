ba889f135fdce432c9ffda25e7be2cb4ce1a9d25
nico- <nico.schneider-2@mni.thm.de>
2023-05-15 14:02:48 +0200

    customized drawer navigator

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import {StyleSheet} from "react-native";
-import Profile from "./screens/Profile";
+import {createDrawerNavigator} from "@react-navigation/drawer";
 import {NavigationContainer} from "@react-navigation/native";
 import newPost from "./screens/NewPost";
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import Profile from "./screens/Profile";
+import Sidebar from "./components/Sidebar";
+import {Feather} from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Drawer.Navigator screenOptions={{headerShown: false}}>
+            <Drawer.Navigator screenOptions={{headerShown: false}} drawerContent={props => <Sidebar {...props}/>}>
                 <Drawer.Screen
                     name="Profile"
                     component={Profile}
+                    options={{
+                        drawerIcon: ({tintColor}) => <Feather name="user" size={16} color={tintColor}/>
+                    }}
                 />
                 <Drawer.Screen
                     name="NewPost"
                     component={newPost}
+                    options={{
+                        drawerIcon: ({tintColor}) => <Feather name="file-plus" size={16} color={tintColor}/>
+                    }}
                 />
             </Drawer.Navigator>
         </NavigationContainer>
