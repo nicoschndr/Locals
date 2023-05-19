99696ade86afedb2c94c285767fa56e0889adf69
sebastian <sebastian.waldschmidt-2@mni.thm.de>
2023-05-19 02:21:12 +0200

    Freundesliste auskommentiert, weil stresst rum in der um halb 3 nachts. Ist eine Aufgabe für Zukunfts-Ich das zu fixen. Bis dahin -> auskommentiert.

--- screens/appScreens/FriendList.jsx
+++ screens/appScreens/FriendList.jsx
@@ -1,3 +1,74 @@
+/*
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import {firebase} from "../../firebase";
+const FriendsScreen = () => {
+	const [friends, setFriends] = useState([]);
+
+	useEffect(() => {
+		// Firebase-Datenbankreferenz für die Freundesliste
+		const friendsRef = firebase.database().ref('friends');
+
+		// Freundesliste abrufen
+		friendsRef.on('value', (snapshot) => {
+			if (snapshot.exists()) {
+				const friendsData = snapshot.val();
+				// Die Freundesliste in den State setzen
+				setFriends(friendsData);
+			}
+		});
+
+		// Aufräumen beim Komponentenabbau
+		return () => {
+			friendsRef.off('value');
+		};
+	}, []);
+
+	// Rendermethode für jedes Freundesobjekt in der FlatList
+	const renderFriendItem = ({ item }) => {
+		return (
+			<View style={styles.friendItem}>
+				<Text>{item.name}</Text>
+				<Text>{item.email}</Text>
+			</View>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.heading}>Freundesliste</Text>
+			<FlatList
+				data={friends}
+				renderItem={renderFriendItem}
+				keyExtractor={(item) => item.id}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 20,
+	},
+	heading: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 10,
+	},
+	friendItem: {
+		marginBottom: 10,
+	},
+});
+
+export default FriendsScreen;
+
+ */
+
+
+
 import { View, Text, Button, StyleSheet } from "react-native";
 import React, { useState } from "react";
 
@@ -6,7 +77,7 @@ const FriendScreen = () => {
 	// render related stuff is displayed here after the return statement
 	return (
 		<View>
-			<Text>Home Screen</Text>
+			<Text>Freundes Liste (auskommentiert, weil muckt dieser Huso)</Text>
 		</View>
 	);
 };
