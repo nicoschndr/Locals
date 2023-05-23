fc7d5d6418917fbcf0f466f295db0a1d0c801025
sebastian <sebastian.waldschmidt-2@mni.thm.de>
2023-05-23 23:25:54 +0200

    Funktionalität um seine teilnahme an einem event aufzuzeigen hinzugefügt

--- screens/appScreens/LiveMap.jsx
+++ screens/appScreens/LiveMap.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Animated, Easing, View, StyleSheet } from 'react-native';
+import { Animated, Easing, View, StyleSheet, Modal, TouchableOpacity, Linking, Platform } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
-import {auth, firebase, firestore, storage} from "../../firebase";
-
-
-
+import { auth, firebase } from "../../firebase";
+import { Text, Image } from "react-native";
+import LocalsButton from "../../components/LocalsButton";
 
 const AnimatedCircle = ({ style }) => {
 	const scaleAnimation = useRef(new Animated.Value(0)).current;
@@ -48,6 +47,79 @@ const AnimatedCircle = ({ style }) => {
 const Livemap = () => {
 	const [location, setLocation] = useState(null);
 	const [events, setEvents] = useState([]);
+	const [modalVisible, setModalVisible] = useState(false);
+	const [selectedEvent, setSelectedEvent] = useState({ attendees: [] });
+	const { user } = auth.currentUser;
+
+	const getUsername = async () => {
+		const user = auth.currentUser;
+		if (!user) {
+			throw new Error('User not authenticated');
+		}
+		const userRef = firebase.firestore().collection('users').doc(user.uid);
+		const userDoc = await userRef.get();
+		const username = userDoc.data().username;
+		return username;
+	};
+
+
+	const handleEventPress = (event) => {
+		setSelectedEvent(event);
+		setModalVisible(true);
+	};
+
+	const attendEvent = async () => {
+		const user = firebase.auth().currentUser;
+		const username = await getUsername();
+
+		if (user && selectedEvent) {
+			const eventRef = firebase.firestore().collection('events').doc(selectedEvent.id);
+
+			await eventRef.update({
+				attendees: firebase.firestore.FieldValue.arrayUnion(username),
+				groupSize: firebase.firestore.FieldValue.increment(1)
+			});
+
+			setSelectedEvent(prevState => ({
+				...prevState,
+				attendees: [...prevState.attendees, username],
+				groupSize: prevState.groupSize + 1,
+				isAttending: true,
+			}));
+		}
+	};
+
+	const cancelAttendance = async () => {
+		const user = firebase.auth().currentUser;
+		const username = await getUsername();
+
+		if (user && selectedEvent) {
+			const eventRef = firebase.firestore().collection('events').doc(selectedEvent.id);
+
+			await eventRef.update({
+				attendees: firebase.firestore.FieldValue.arrayRemove(username),
+				groupSize: firebase.firestore.FieldValue.increment(-1)
+			});
+
+			setSelectedEvent(prevState => ({
+				...prevState,
+				attendees: prevState.attendees.filter(user => user !== username),
+				groupSize: prevState.groupSize - 1,
+				isAttending: false,
+			}));
+		}
+	};
+
+
+	const toggleAttendance = async () => {
+		const username = await getUsername();
+
+		if (selectedEvent && selectedEvent.attendees && selectedEvent.attendees.includes(username)) {
+			cancelAttendance();
+		} else {
+			attendEvent();
+		}
+	};
 
 	useEffect(() => {
 		(async () => {
@@ -74,23 +146,39 @@ const Livemap = () => {
 		})();
 	}, []);
 
+	const openMapsApp = () => {
+		const { latitude, longitude } = selectedEvent;
+
+		const url = Platform.select({
+			ios: `maps:0,0?q=${latitude},${longitude}`,
+			android: `geo:0,0?q=${latitude},${longitude}`,
+		});
+
+		Linking.openURL(url);
+	};
+
 	useEffect(() => {
-		// Fetch events from Firebase Firestore
 		const fetchEvents = async () => {
+			const username = await getUsername();
 			const eventsRef = firebase.firestore().collection('events');
-			const snapshot = await eventsRef.get();
 
-			const eventsData = snapshot.docs.map((doc) => ({
-				id: doc.id,
-				...doc.data(),
-			}));
+			eventsRef.onSnapshot((snapshot) => {
+				const eventsData = snapshot.docs.map((doc) => {
+					const event = {
+						id: doc.id,
+						...doc.data(),
+					};
 
-			setEvents(eventsData);
+					event.isAttending = Array.isArray(event.attendees) && event.attendees.includes(username);
+					return event;
+				});
+
+				setEvents(eventsData);
+			});
 		};
 
 		fetchEvents();
 	}, []);
-
 	return (
 		<View style={styles.container}>
 			{location && (
@@ -119,14 +207,41 @@ const Livemap = () => {
 								latitude: event.latitude,
 								longitude: event.longitude,
 							}}
-							title={event.title}
-							description={event.address}
+							onPress={() => handleEventPress(event)}
 						>
 							<View style={styles.eventMarker} />
 						</Marker>
 					))}
 				</MapView>
 			)}
+			<Modal visible={modalVisible} animationType="slide">
+				<View style={styles.modalContainer}>
+					{selectedEvent && (
+						<View style={{ flex: 1, alignItems: "center" }}>
+							<Image style={styles.eventModalImage} source={{ uri: selectedEvent.imageUrl }} />
+							<Text style={styles.eventModalTitle}>{selectedEvent.title}</Text>
+							<Text style={styles.eventModalText}>Erstellt von: {selectedEvent.creator}</Text>
+							<LocalsButton title={"Hin da!"} onPress={openMapsApp} />
+							<Text style={styles.eventModalText}>Beschreibung: {selectedEvent.description}</Text>
+							<Text style={styles.eventModalText}>Wer ist eingeladen: {selectedEvent.gender}</Text>
+							<Text style={styles.eventModalText}>Kategorie: {selectedEvent.category}</Text>
+
+							{selectedEvent.isAttending ? (
+								<LocalsButton title={"Nicht teilnehmen"} onPress={toggleAttendance} />
+							) : (
+								<LocalsButton title={"Teilnehmen"} onPress={toggleAttendance} />
+							)}
+
+
+							<Text style={styles.eventModalText}>Group Size: {selectedEvent.groupSize}</Text>
+						</View>
+					)}
+
+					<TouchableOpacity onPress={() => setModalVisible(false)}>
+						<Text style={styles.closeButton}>Close</Text>
+					</TouchableOpacity>
+				</View>
+			</Modal>
 		</View>
 	);
 };
@@ -148,17 +263,56 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-	innerCircle: {
-		width: 8,
-		height: 8,
-		borderRadius: 4,
-		backgroundColor: 'blue',
+	eventContainer: {
+		alignItems: 'center',
 	},
 	eventMarker: {
 		width: 16,
 		height: 16,
 		borderRadius: 8,
 		backgroundColor: 'red',
+	},
+	eventTitle: {
+		fontSize: 12,
+		color: 'black',
+		marginTop: 4,
+		textAlign: 'center',
+	},
+	modalContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	eventModalTitle: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 8,
+	},
+	eventModalText: {
+		fontSize: 16,
+		marginBottom: 8,
+	},
+	eventModalImage: {
+		width: 200,
+		height: 200,
+		resizeMode: 'cover',
+		marginBottom: 16,
+	},
+	closeButton: {
+		fontSize: 16,
+		color: 'blue',
+		marginTop: 20,
+	},
+	mapButton: {
+		backgroundColor: 'blue',
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		borderRadius: 4,
+		marginBottom: 16,
+	},
+	mapButtonText: {
+		color: 'white',
+		fontSize: 16,
 	},
 });
 
