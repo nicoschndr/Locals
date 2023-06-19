0fbd982b7fdcb56a6da1b4808393618368ca900b
janoleschmidt <janoleschmidt@outlook.com>
2023-06-19 15:36:11 +0200

    added explore header

--- components/LocalsEventCard.tsx
+++ components/LocalsEventCard.tsx
@@ -1,64 +1,66 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 interface LocalsEventCardProps {
-    title: string;
-    date: string;
-    location: string;
-    image: string;
-    onPress: () => void;
+	title: string;
+	date: string;
+	location: string;
+	image: string;
+	onPress: () => void;
 }
 
 const LocalsEventCard = (props: LocalsEventCardProps) => {
-
-    return (
-        <TouchableOpacity style={styles.container} onPress={props.onPress}>
-            <Image source={{ uri: props.image }} style={styles.image} />
-            <View style={styles.detailsContainer}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.date}>{props.date}</Text>
-                <Text style={styles.location}>{props.location}</Text>
-            </View>
-        </TouchableOpacity >
-    );
-}
+	return (
+		<TouchableOpacity style={styles.container} onPress={props.onPress}>
+			<Image source={{ uri: props.image }} style={styles.image} />
+			<View style={styles.detailsContainer}>
+				<Text style={styles.title}>{props.title}</Text>
+				{props.date && <Text style={styles.date}>{props.date}</Text>}
+				{props.location && (
+					<Text style={styles.location}>{props.location}</Text>
+				)}
+			</View>
+		</TouchableOpacity>
+	);
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        overflow: 'hidden',
-        marginVertical: 10,
-        marginHorizontal: 20,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    image: {
-        height: 100,
-        width: 100,
-    },
-    detailsContainer: {
-        flex: 1,
-        padding: 10,
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    date: {
-        fontSize: 16,
-        marginTop: 5,
-    },
-    location: {
-        fontSize: 16,
-        marginTop: 5,
-    },
+	container: {
+		flexDirection: "row",
+		alignItems: "center",
+		backgroundColor: "#fff",
+		borderRadius: 10,
+		overflow: "hidden",
+		marginBottom: 20,
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+		height: 100,
+	},
+	image: {
+		height: 100,
+		width: 100,
+		resizeMode: "cover",
+	},
+	detailsContainer: {
+		flex: 1,
+		paddingHorizontal: 10,
+	},
+	title: {
+		fontSize: 16,
+		fontWeight: "bold",
+	},
+	date: {
+		fontSize: 12,
+		marginTop: 5,
+		color: "#888",
+	},
+	location: {
+		fontSize: 14,
+		marginTop: 5,
+	},
 });
 
 export default LocalsEventCard;
--- screens/appScreens/Home.jsx
+++ screens/appScreens/Home.jsx
@@ -1,16 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, TextInput, StyleSheet, RefreshControl } from 'react-native';
-import { firestore } from '../../firebase';
+import React, { useState, useEffect } from "react";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	ScrollView,
+	TextInput,
+	StyleSheet,
+	Image,
+	RefreshControl,
+} from "react-native";
+import { firestore } from "../../firebase";
 
-import LocalsEventCard from '../../components/LocalsEventCard';
-import { Ionicons } from '@expo/vector-icons';
+import LocalsEventCard from "../../components/LocalsEventCard";
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = ({ navigation }) => {
 	const [users, setUsers] = useState([]);
-	const [search, setSearch] = useState('');
+	const [search, setSearch] = useState("");
 	const [events, setEvents] = useState([""]);
 	const [refreshing, setRefreshing] = useState(false);
-
+	const [showSearch, setShowSearch] = useState(false);
 
 	useEffect(() => {
 		getUsers();
@@ -31,15 +40,13 @@ const HomeScreen = ({ navigation }) => {
 	}
 
 	const getAllEvents = () => {
-		firestore
-			.collection("events")
-			.onSnapshot((snapshot) => {
-				const events = snapshot.docs.map((doc) => ({
-					id: doc.id,
-					...doc.data(),
-				}));
-				setEvents(events);
-			});
+		firestore.collection("events").onSnapshot((snapshot) => {
+			const events = snapshot.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}));
+			setEvents(events);
+		});
 	};
 	const handleRefresh = () => {
 		setRefreshing(true);
@@ -47,55 +54,75 @@ const HomeScreen = ({ navigation }) => {
 		setRefreshing(false);
 	};
 
+	const filterEvents = (events, search) => {
+		return events.filter((event) => {
+			const date = event.date?.toDate()?.toLocaleDateString();
+			// date by format: january 1, 2021
+			const byMonth = event.date?.toDate()?.toLocaleDateString("de-DE", {
+				month: "long",
+			});
+			const byTitleLowerCase =
+				`${event.title},${event.address},${date},${event.category},${byMonth}`.toLowerCase();
+			const searchByTitleLowerCase = search.toLowerCase();
+
+			return byTitleLowerCase.includes(searchByTitleLowerCase);
+		});
+	};
+
+	const FilteredEvents = filterEvents(events, search);
+
 	return (
 		<View style={styles.container}>
-			<View style={{ flexDirection: 'row', justifyContent: "space-between", alignItems: 'center', justifyContent: 'center' }}>
-				<TextInput
-					style={styles.searchInput}
-					value={search}
-					placeholder="Search Events"
-					onChangeText={setSearch}
-				/>
-				{/* go to post event */}
+			<View style={styles.header}>
+				<Image source={require("../../assets/Logo.png")} style={styles.logo} />
+				<View style={{ flexDirection: "row", alignItems: "center" }}>
+					{!showSearch ? (
+						<TouchableOpacity
+							style={styles.searchButton}
+							onPress={() => setShowSearch(!showSearch)}
+						>
+							<Ionicons
+								name="search-outline"
+								size={28}
+								color="black"
+								// make icon bold
+								style={{ fontWeight: "bold" }}
+							/>
+						</TouchableOpacity>
+					) : (
+						<TextInput
+							style={styles.searchInput}
+							value={search}
+							placeholder="Search Events"
+							onChangeText={setSearch}
+							onSubmitEditing={() => setShowSearch(!showSearch)}
+						/>
+					)}
 
-				<TouchableOpacity
-					style={styles.postButton}
-					onPress={() => navigation.navigate("PostEvent")}
-				>
-					<Ionicons
-						name="add-circle-outline"
-						size={36}
-						color="black"
-					/>
-				</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.postButton}
+						onPress={() => navigation.navigate("PostEvent")}
+					>
+						<Ionicons name="add-circle-outline" size={28} color="black" />
+					</TouchableOpacity>
+				</View>
 			</View>
 			<ScrollView
 				refreshControl={
 					<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
 				}
+				style={{ marginTop: 24, marginHorizontal: 24 }}
 			>
-				{events
-					.filter((events) => {
-						const date = events.date?.toDate()?.toLocaleDateString();
-						// date by format: january 1, 2021
-						const byMonth = events.date?.toDate()?.toLocaleDateString("de-DE", {
-							month: "long",
-						});
-						const byTitleLowerCase = `${events.title},${events.address},${date},${events.category},${byMonth}`.toLowerCase();
-						const searchByTitleLowerCase = search.toLowerCase();
-
-						return byTitleLowerCase.includes(searchByTitleLowerCase);
-					})
-					.map((event) => (
-						<LocalsEventCard
-							key={event.id}
-							title={event.title}
-							date={event.date?.toDate()?.toLocaleDateString()}
-							location={event.address}
-							image={event.imageUrl}
-							onPress={() => navigation.navigate("EventDetails", { event })}
-						/>
-					))}
+				{FilteredEvents.map((event) => (
+					<LocalsEventCard
+						key={event.id}
+						title={event.title}
+						date={event.date?.toDate()?.toLocaleDateString()}
+						location={event.address}
+						image={event.imageUrl}
+						onPress={() => navigation.navigate("EventDetails", { event })}
+					/>
+				))}
 			</ScrollView>
 		</View>
 	);
@@ -104,21 +131,36 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		marginTop: 50,
-		padding: 10,
+		backgroundColor: "#fff",
+	},
+	header: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginTop: 48,
+		marginHorizontal: 24,
 	},
 	searchInput: {
-		height: 40,
-		width: '75%',
-		borderColor: '#000',
+		height: 30,
+		width: "75%",
+		borderColor: "#000",
 		borderWidth: 1,
 		paddingLeft: 10,
 		borderRadius: 30,
 	},
 	postButton: {
 		marginLeft: 10,
-
-	}
+	},
+	searchButton: {
+		marginLeft: 10,
+	},
+	logo: {
+		height: 40,
+		// adjust styling for logo
+		width: 100,
+		resizeMode: "contain",
+		left: -10,
+	},
 });
 
 export default HomeScreen;
