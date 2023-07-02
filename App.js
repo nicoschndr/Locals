39c7cf791e3c9b4f133d0796d4a844394e5ccb7b
nico- <nico.schneider-2@mni.thm.de>
2023-07-02 15:51:44 +0200

    small fix

--- App.js
+++ App.js
@@ -4,6 +4,19 @@ import React, { useEffect, useState } from "react";
 import FirestoreProvider from "./context/FirestoreProvider";
 LogBox.ignoreAllLogs();
 export default function App() {
+	//navigationOptions = {
+	//	headerShown: true,
+	//	style: {
+	//		backgroundColor: "transparent",
+	//	},
+	//};
+	// useEffect(async () => {
+	// 	const defaultApp = initializeApp(firebaseConfig);
+	// 	initializeAuth(defaultApp, {
+	// 		persistence: getReactNativePersistence(AsyncStorage),
+	// 	});
+	// }, []);
+
 	return (
 		<FirestoreProvider>
 			<AppNavigation />
