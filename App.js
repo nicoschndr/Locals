e659dad2f39ccfed7ac15806ffcf3a62eb07f15a
nico- <nico.schneider-2@mni.thm.de>
2023-07-02 15:08:22 +0200

    small fix

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { Dimensions, StyleSheet, Text, View, LogBox } from "react-native";
 import AppNavigation from "./screens/AppNavigation";
 import React, { useEffect, useState } from "react";
+import FirestoreProvider from "./context/FirestoreProvider";
 LogBox.ignoreAllLogs();
 export default function App() {
-	return <AppNavigation />;
+	return (
+		<FirestoreProvider>
+			<AppNavigation />
+		</FirestoreProvider>
+	);
 }
 
 const styles = StyleSheet.create({});
