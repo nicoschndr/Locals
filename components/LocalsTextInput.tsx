17b4754da3ca985cc92d55a15f7d93333308b4f9
nico- <nico.schneider-2@mni.thm.de>
2023-07-08 15:42:58 +0200

    ApiDoc of the components.

--- components/LocalsTextInput.tsx
+++ components/LocalsTextInput.tsx
@@ -8,6 +8,9 @@ import {
 import React, { useState } from "react";
 import { TextInput } from "react-native-gesture-handler";
 
+/**
+ * This interface defines the props (properties) accepted by the LocalsTextInput component.
+ */
 export interface LocalsTextInputProps {
 	placeholder: string;
 	value: string;
@@ -21,6 +24,19 @@ export interface LocalsTextInputProps {
 	textColor: string;
 }
 
+/**
+ * Renders a LocalsTextInput component with the provided props.
+ * @param placeholder - Placeholder text for the input.
+ * @param autoFocus - Indicates whether the input should be focused when rendered.
+ * @param inputMode - The input mode for mobile keyboards (e.g., "text", "numeric", "email").
+ * @param value - The current value of the input.
+ * @param onChangeText - Callback function invoked when the text in the input changes.
+ * @param style - Additional style for the input component.
+ * @param secureTextEntry - Indicates whether the input should be displayed as a secure text entry.
+ * @param autoCapitalize - Specifies how the text should be capitalized in the input.
+ * @param textColor - The color of the text in the input.
+ * @constructor
+ */
 const LocalsTextInput: React.FC<LocalsTextInputProps> = ({
 	placeholder,
 	autoFocus,
@@ -32,6 +48,10 @@ const LocalsTextInput: React.FC<LocalsTextInputProps> = ({
 	autoCapitalize = "none",
 	textColor,
 }) => {
+
+	/**
+	 * Renders the LocalsTextInput component.
+	 */
 	return (
 		<TextInput
 			placeholder={placeholder}
@@ -47,6 +67,9 @@ const LocalsTextInput: React.FC<LocalsTextInputProps> = ({
 	);
 };
 
+/**
+ * Creates a StyleSheet object containing style definitions for component.
+ */
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: "#fff",
